324b7fdf9b7a216e2f6259d09b736fdb7d60cf0e
s0n1k <team@litecoinz.info>
2018-02-02 01:07:13 +0100

    20180202010707

--- depends/packages/libSM.mk
+++ depends/packages/libSM.mk
@@ -1,12 +1,13 @@
-package=xtrans
-$(package)_version=1.3.5
+package=libSM
+$(package)_version=1.2.2
 $(package)_download_path=http://xorg.freedesktop.org/releases/individual/lib/
 $(package)_file_name=$(package)-$($(package)_version).tar.bz2
-$(package)_sha256_hash=adbd3b36932ce4c062cd10f57d78a156ba98d618bdb6f50664da327502bc8301
-$(package)_dependencies=
+$(package)_sha256_hash=0baca8c9f5d934450a70896c4ad38d06475521255ca63b717a6510fdb6e287bd
+$(package)_dependencies=xtrans xproto libICE
 
 define $(package)_set_vars
-$(package)_config_opts_linux=--with-pic --disable-static
+  $(package)_config_opts=--without-libuuid  --without-xsltproc  --disable-docs --disable-static
+  $(package)_config_opts_linux=--with-pic
 endef
 
 define $(package)_config_cmds
--- depends/packages/protobuf.mk
+++ depends/packages/protobuf.mk
@@ -4,7 +4,7 @@ $(package)_download_path=$(native_$(package)_download_path)
 $(package)_file_name=$(native_$(package)_file_name)
 $(package)_sha256_hash=$(native_$(package)_sha256_hash)
 $(package)_dependencies=native_$(package)
-$(package)_cxxflags=-std=c++11
+$(package)_cxxflags=-std=c++11 -Wno-unused-local-typedefs -Wno-return-type -Wno-maybe-uninitialized -Wno-misleading-indentation -Wno-unused-function
 
 define $(package)_set_vars
   $(package)_config_opts=--disable-shared --with-protoc=$(build_prefix)/bin/protoc
@@ -12,7 +12,7 @@ define $(package)_set_vars
 endef
 
 define $(package)_config_cmds
-  $($(package)_autoconf)
+  $($(package)_autoconf) --disable-dependency-tracking
 endef
 
 define $(package)_build_cmds
